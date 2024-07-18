5de4ffaaf505bd105419157c5526a41ef6e60285
gg-gyaniguru <gg.gyaniguru@gmail.com>
2024-07-18 13:12:49 +0530

    backdrops-api

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connection = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URI as string);
+        await mongoose.connect(Bun.env.MONGODB_URI as string);
     } catch (error) {
         console.error('error in connecting to database');
     }
