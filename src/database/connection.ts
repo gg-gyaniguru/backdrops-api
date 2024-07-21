4b63f9f78d830d6815604b3e1c9e54fa6983ade1
gg-gyaniguru <gg.gyaniguru@gmail.com>
2024-07-21 13:40:28 +0530

    set database

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connection = async () => {
     try {
-        await mongoose.connect('mongodb://localhost:27017/backdrops');
+        await mongoose.connect(process.env.MONGODB_URI as string);
     } catch (error) {
         console.error('error in connecting to database');
     }
